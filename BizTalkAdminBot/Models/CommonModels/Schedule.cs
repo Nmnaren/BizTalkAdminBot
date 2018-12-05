6d2455e07615ec31c4456f179ee378e671edb121
mandardhikari <mandardhikari@gmail.com>
2018-12-05 23:50:26 +1030

    Added Model for the Receive Location

--- BizTalkAdminBot/Models/CommonModels/Schedule.cs
+++ BizTalkAdminBot/Models/CommonModels/Schedule.cs
@@ -4,6 +4,10 @@ namespace BizTalkAdminBot.Models
 {
     public class Schedule
     {
+        public DateTime StartDate { get; set; }
+        public bool StartDateEnabled { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool EndDateEnabled { get; set; }
         public bool ServiceWindowEnabled { get; set; }
         public DateTime FromTime { get; set; }
         public DateTime ToTime { get; set; }
---
+++ BizTalkAdminBot/Models/ReceiveLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizTalkAdminBot.Models
+{
+    public class ReceiveLocation
+    {
+        public string Name { get; set; }
+        public object Description { get; set; }
+        public string ReceivePortName { get; set; }
+        public bool Enable { get; set; }
+        public bool IsPrimary { get; set; }
+        public string Address { get; set; }
+        public string PublicAddress { get; set; }
+        public string TransportType { get; set; }
+        public string TransportTypeData { get; set; }
+        public string ReceiveHandler { get; set; }
+        public object CustomData { get; set; }
+        public string ReceivePipeline { get; set; }
+        public object ReceivePipelineData { get; set; }
+        public string SendPipeline { get; set; }
+        public object SendPipelineData { get; set; }
+        public Schedule Schedule { get; set; }
+        public object EncryptionCert { get; set; }
+        public string FragmentMessages { get; set; }
+    }
+}
